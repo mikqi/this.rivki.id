4ad95a14809db415f31867e97ceee396b82ef535
Muhammad Rivki <Muhammad.Rivki@live.com>
2019-06-04 16:01:46 +0700

    update homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,62 +1,60 @@
 <template>
   <section class="section hero">
-    <div class="container u-full-width u-full-height flex">
-      <div class="row u-full-width self-center">
-        <div class="twelve columns">
-          <p class="u-p--hero mb5">
-            Hello,
-          </p>
-          <h1 class="hero-heading ma0">
-            I'm Muhammad Rivki,
-          </h1>
-          <h2 class="ma0">
-            Frontend Engineer at&nbsp;<a
-              href="https://bukalapak.com"
-              title="Bukalapak"
-            >
-              Bukalapak
-            </a>
-          </h2>
-          <h2 class="ma0">
-            who love
-            <span
-              class="txt-rotate"
-              data-period="5000"
-              data-rotate="[ &quot;JavaScript.&quot;, &quot;chocolate.&quot;, &quot;classical music.&quot;, &quot;of course cat!!!!&quot; ]"
-            />
-          </h2>
-          <p class="mt5 u-p--hero">
-            I spend most of my day working with the frontend things and read articles at feedly. You can find me on <a
-              href="https://facebook.com/this.mikqi"
-              target="_blank"
-              class="facebook"
-            >
-            Facebook</a>,
-            <a
-              href="https://instagram.com/mikqii"
-              target="_blank"
-              class="github"
-            >
-            Instagram</a>,
-            <a
-              href="https://twitter.com/__mikqi"
-              target="_blank"
-              class="twitter"
-            >
-            Twitter</a>,
-            <a
-              href="https://www.linkedin.com/in/muhammadrivki/"
-              target="_blank"
-              class="linkedin"
-            >
-            LinkedIn</a> and
-            <a
-              href="https://github.com/mikqi"
-              target="_blank"
-              class="github"
-            >GitHub</a>.
-          </p>
-        </div>
+    <div class="flex vh-100 w-60 center justify-center items-center">
+      <div>
+        <p class="f2 fw1 mb4">
+          Hello.
+        </p>
+        <h1 class="f1 ma0">
+          I'm Muhammad Rivki,
+        </h1>
+        <h2 class="f2 fw1 ma0 lh-copy">
+          Frontend Engineer at&nbsp;<a
+            href="https://bukalapak.com"
+            title="Bukalapak"
+          >
+            Bukalapak
+          </a>
+        </h2>
+        <h2 class="f2 fw1 ma0 lh-copy">
+          who love
+          <span
+            class="txt-rotate"
+            data-period="5000"
+            data-rotate="[ &quot;JavaScript.&quot;, &quot;chocolate.&quot;, &quot;classical music.&quot;, &quot;of course cat!!!!&quot; ]"
+          />
+        </h2>
+        <p class="f4 mt4 fw1 lh-copy tracked-sm">
+          I spend most of my day working with the frontend things and read articles at feedly. You can find me on <a
+            href="https://facebook.com/this.mikqi"
+            target="_blank"
+            class="facebook"
+          >
+          Facebook</a>,
+          <a
+            href="https://instagram.com/mikqii"
+            target="_blank"
+            class="github"
+          >
+          Instagram</a>,
+          <a
+            href="https://twitter.com/__mikqi"
+            target="_blank"
+            class="twitter"
+          >
+          Twitter</a>,
+          <a
+            href="https://www.linkedin.com/in/muhammadrivki/"
+            target="_blank"
+            class="linkedin"
+          >
+          LinkedIn</a> and
+          <a
+            href="https://github.com/mikqi"
+            target="_blank"
+            class="github"
+          >GitHub</a>.
+        </p>
       </div>
     </div>
   </section>

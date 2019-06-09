b9e8a5910238f1e16c3314be21f57bbb252fc75d
Muhammad Rivki <Muhammad.Rivki@live.com>
2019-06-09 22:57:15 +0700

    change color footer text heart to yellow

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="flex justify-center pt5 pb3">
     <p class="ma0 flex flex-column items-center">
       <span>
-        Built with <span class="love">
+        Built with <span class="love yellow">
           ❤
         </span>︎ and some chocolates
       </span>

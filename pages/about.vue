06d120d2ff6de2da0065fa103386db514677c9e5
Muhammad Rivki <Muhammad.Rivki@live.com>
2019-06-09 22:51:06 +0700

    fix margin and padding

--- pages/about.vue
+++ pages/about.vue
@@ -10,7 +10,7 @@
     <div>
       <!-- MAIN CONTENT ABOUT -->
       <div class="flex flex-column vh-100-l vh-100-m w-70-m w-50-l w-90-ns center justify-center items-center-l items-center-m">
-        <div class="flex flex-column-ns flex-row-m flex-row-l mb4 mb0-ns mt6-ns">
+        <div class="flex flex-column-ns flex-row-m flex-row-l mb4 mb4-l mb0-ns mt6-ns mt0-l mt0-m">
           <img
             class="avatar self-center-ns"
             src="@/static/img/ava.jpg"
@@ -44,15 +44,15 @@
       <!-- END MAIN HEAD ABOUT -->
 
       <!-- EXPERIENCES AND ACHIEVEMENTS -->
-      <div>
+      <div class="mt5-ns mt0-l mt0-m">
         <!-- EXPERIENCES -->
         <div class="flex flex-column w-70-m w-50-l w-90-ns center justify-start items-start-l items-start-m mb4">
           <h5 class="ma0 mb4 f3 self-start">
             <em>– EXPERIENCES</em>
           </h5>
-          <div class="flex-l flex-m mb3">
+          <div class="flex-l flex-m mb3-l mb3-m">
             <!--BUKALAPAK.COM FTL-->
-            <div class="lh0 mb3 w-50-l w-50-m">
+            <div class="lh0 mb3-m mb3-l mb4-ns w-50-l w-50-m">
               <div class="lh-copy light-silver f6">
                 Oct 2018 - Today
               </div>
@@ -64,8 +64,8 @@
                   <em>Frontend Tribe Lead</em>
                 </small>
               </div>
-              <div class="mt3 lh-copy f4 fw1 pr2">
-                <small>
+              <div class="mt2 lh-copy f4 fw1 pr4">
+                <small class="black-70">
                   Bukalapak use <a
                     target="_blank"
                     href="https://medium.com/productmanagement101/spotify-squad-framework-part-i-8f74bcfcd761">Spotify Engineering Culture</a> and on this position I focus to remove knowledge gap with other tribes and to keep my Tribe code quality is good.
@@ -74,7 +74,7 @@
             </div>
 
             <!--BUKALAPAK.COM FTL-->
-            <div class="lh0 mb3 w-50-l w-50-m">
+            <div class="lh0 mb3-m mb3-l mb4-ns w-50-l w-50-m">
               <div class="lh-copy light-silver f6">
                 Apr 2018 - Today
               </div>
@@ -86,17 +86,17 @@
                   <em>Frontend Engineer Squad Lead</em>
                 </small>
               </div>
-              <div class="mt3 lh-copy f4 fw1 pr2">
-                <small>
+              <div class="mt2 lh-copy f4 fw1 pr4">
+                <small class="black-70">
                   As a lead i responsible to made my squad code quality better with code reviews and also handle for deployment my squad product to production with less bug.
                 </small>
               </div>
             </div>
           </div>
 
-          <div class="flex-l flex-m mb3">
+          <div class="flex-l flex-m mb3-l mb3-m">
             <!--BUKALAPAK.COM-->
-            <div class="lh0 mb3 w-50-l w-50-m">
+            <div class="lh0 mb3-m mb3-l mb4-ns w-50-l w-50-m">
               <div class="lh-copy light-silver f6">
                 Oct 2016 - Today
               </div>
@@ -108,8 +108,8 @@
                   <em>Frontend Engineer</em>
                 </small>
               </div>
-              <div class="mt3 lh-copy f4 fw1 pr2">
-                <small>
+              <div class="mt2 lh-copy f4 fw1 pr4">
+                <small class="black-70">
                   In Bukalapak I focus built and maintenance at seller side but before transactions such as <em>create product</em>, <em>product list</em> etc. I develop previously using jQuery and Coffescript, but now already use <a
                     target="_blank"
                     href="https://vuejs.org">VueJS</a>.
@@ -118,7 +118,7 @@
             </div>
 
             <!-- ShoutCap -->
-            <div class="lh0 mb3 w-50-l w-50-m">
+            <div class="lh0 mb3-m mb3-l mb4-ns w-50-l w-50-m">
               <div class="lh-copy light-silver f6">
                 Jul 2015 - Sep 2015
               </div>
@@ -130,8 +130,8 @@
                   <em>Frontend Engineer Intern</em>
                 </small>
               </div>
-              <div class="mt3 lh-copy f4 fw1 pr2">
-                <small>
+              <div class="mt2 lh-copy f4 fw1 pr4">
+                <small class="black-70">
                   I develop iOS ShoutCap App using <a
                     target="_blank"
                     href="http://ionicframework.com">Ionic Framework</a> and create a Profile, Voucher, Transactions and Create a Cap Views.
@@ -140,9 +140,9 @@
             </div>
           </div>
 
-          <div class="flex-l flex-m mb3">
+          <div class="flex-l flex-m mb3-l mb3-m">
             <!--CODELABS.COM-->
-            <div class="lh0 mb3 w-50-l w-50-m">
+            <div class="lh0 mb3-m mb3-l mb4-ns w-50-l w-50-m">
               <div class="lh-copy light-silver f6">
                 Dec 2012 - Sep 2016
               </div>
@@ -154,8 +154,8 @@
                   <em>Fullstack Javascript</em>
                 </small>
               </div>
-              <div class="mt3 lh-copy f4 fw1 pr2">
-                <small>
+              <div class="mt2 lh-copy f4 fw1 pr4">
+                <small class="black-70">
                   In Codelabs i do some reasearch for competition focus on Javascript technology and maintenance some internal websites.
                 </small>
               </div>
@@ -175,45 +175,45 @@
             </h5>
 
             <!--Seller Center-->
-            <div class="lh0 mb3">
+            <div class="lh0 mb4">
               <div class="lh-copy light-silver f6">
                 October 2018
               </div>
               <div class="lh-copy mv1">
                 Component Library Bukalapak
               </div>
-              <div class="mt3 lh-copy f4 fw1">
-                <small>
+              <div class="mt2 lh-copy f4 fw1">
+                <small class="black-70">
                   Parallel with develop Seller Center, I and my team designing reuseable components from Seller Center. After that we move it to our internal npm registry to share with other engineer and now the project has been moved to Core Frontend.
                 </small>
               </div>
             </div>
 
             <!--Seller Center-->
-            <div class="lh0 mb3">
+            <div class="lh0 mb4">
               <div class="lh-copy light-silver f6">
                 March 2018
               </div>
               <div class="lh-copy mv1">
                 Seller Center Bukalapak
               </div>
-              <div class="mt3 lh-copy f4 fw1">
-                <small>
+              <div class="mt2 lh-copy f4 fw1">
+                <small class="black-70">
                   I lead the team to develop <a href="https://seller.bukalapak.com">Seller Center Bukalapak</a>. I make standard for the the code to make it clean and also develop the server and client. It used <a href="https://nuxtjs.org">Nuxt.js</a>, so the server side use NodeJS + ExpressJS and the client side use VueJS. This project is recreate features from legacy codebase to the new stack. I responsible for stability and maintainabilty the code.
                 </small>
               </div>
             </div>
 
             <!--7 Pagi Dashboard-->
-            <div class="lh0 mb3">
+            <div class="lh0 mb4">
               <div class="lh-copy light-silver f6">
                 January 2017
               </div>
               <div class="lh-copy mv1">
                 7 Pagi Dashboard
               </div>
-              <div class="mt3 lh-copy f4 fw1">
-                <small>
+              <div class="mt2 lh-copy f4 fw1">
+                <small class="black-70">
                   Create a dashboard for <a
                     href="https://id.techinasia.com/7pagi-platform-interaksi-antara-guru-dan-orang-tua-siswa"
                     target="_blank">7 Pagi</a> startup. I work in backend and frontend side and both of that used JavaScript. NodeJS and Express for backend and AngularJS for frontend also use azure hosting.
@@ -222,15 +222,15 @@
             </div>
 
             <!--Twikipedia-->
-            <div class="lh0 mb3">
+            <div class="lh0 mb4">
               <div class="lh-copy light-silver f6">
                 March 2016
               </div>
               <div class="lh-copy mv1">
                 Twikipedia
               </div>
-              <div class="mt3 lh-copy f4 fw1">
-                <small>
+              <div class="mt2 lh-copy f4 fw1">
+                <small class="black-70">
                   I develop a marketing solution web apps for who runs the busniess in Twitter. I use sentiment analysis and machine learning for categorized the user based on their tweets and the web built with NodeJS for server side and AngularJS for the client. You can read detail about my research <a
                     target="_blank"
                     href="http://jsi.cs.ui.ac.id/index.php/jsi/article/download/500/342/">here</a>.
@@ -239,15 +239,15 @@
             </div>
 
             <!--Dodo Kids-->
-            <div class="lh0 mb3">
+            <div class="lh0 mb4">
               <div class="lh-copy light-silver f6">
                 March 2015
               </div>
               <div class="lh-copy mv1">
                 Dodo Kids Browser
               </div>
-              <div class="mt3 lh-copy f4 fw1">
-                <small>
+              <div class="mt2 lh-copy f4 fw1">
+                <small class="black-70">
                   I create an extension browser for our apps, and i do design too. Dodo Kids Browser is a parenting controlling apps. Which is have two frontend. First in Windows Phone, and second on extension browser.
                 </small>
               </div>
@@ -261,15 +261,15 @@
           <h5 class="ma0 mb4 f3 self-start">
             <em>– ACHIEVEMENTS</em>
           </h5>
-          <div class="lh0 mb3">
+          <div class="lh0 mb4">
             <div class="lh-copy light-silver f6">
               May 2018
             </div>
             <div class="lh-copy mv1">
               Facebook F8 Hakcathon Participation
             </div>
-            <div class="mt3 lh-copy f4 fw1">
-              <small>
+            <div class="mt2 lh-copy f4 fw1">
+              <small class="black-70">
                 I've invited by Facebook to become a participant F8 Hackathon in McEnery Convention Center, San Jose. There was three people in my team with one Backend, Frontend and Business Analyst. And of course the Frontend role is Me. We built an apps focus on helping a community you can check it <a
                   target="_blank"
                   href="https://devpost.com/software/komun-ally">here</a>.
@@ -278,15 +278,15 @@
           </div>
 
           <!--IMAGINE CUP 2017-->
-          <div class="lh0 mb3">
+          <div class="lh0 mb4">
             <div class="lh-copy light-silver f6">
               March 2017
             </div>
             <div class="lh-copy mv1">
               Semi Finalist Microsoft Imagine Cup Indonesia 2017
             </div>
-            <div class="mt3 lh-copy f4 fw1">
-              <small>
+            <div class="mt2 lh-copy f4 fw1">
+              <small class="black-70">
                 I built a marketing solution apps in Twitter use sentiment analysis and categorized the user based on their tweets. With codename <a
                   target="_blank"
                   href="http://sellution.azurewebsites.net/"> Sellution
@@ -300,15 +300,15 @@
           </div>
 
           <!--IMAGINE CUP 2015-->
-          <div class="lh0 mb3">
+          <div class="lh0 mb4">
             <div class="lh-copy light-silver f6">
               May 2015
             </div>
             <div class="lh-copy mv1">
               1<sup>st</sup> Winner Microsoft Imagine Cup Indonesia 2016
             </div>
-            <div class="mt3 lh-copy f4 fw1">
-              <small>
+            <div class="mt2 lh-copy f4 fw1">
+              <small class="black-70">
                 I built parenting control apps that integrate parent device and browser. Parent can see what are kids open in a browser and give an action to block a website if parent doesn't want the kid to open that website. And we win in Innovation Category. We won in first place in <a
                   target="_blank"
                   href="https://dailysocial.id/post/pemenang-imagine-cup-indonesia-2015">
@@ -323,15 +323,15 @@
           </div>
 
           <!--IMAGINE CUP 2018-->
-          <div class="lh0 mb3">
+          <div class="lh0 mb4">
             <div class="lh-copy light-silver f6">
               April 2018
             </div>
             <div class="lh-copy mv1">
               Nominator INAICTA 2014
             </div>
-            <div class="mt3 lh-copy f4 fw1">
-              <small>
+            <div class="mt2 lh-copy f4 fw1">
+              <small class="black-70">
                 We develop apps citizen journalism about crime. With codename <a
                   target="_blank"
                   href="http://if.unikom.ac.id/web/prestasi.php">
